00e9d27febe14c24e1b41cfe857b9868f1e762ad
Rodrigo Leonardo <rodrigoleonardo81@gmail.com>
2022-12-10 15:14:59 -0300

    adding context to my application

---
+++ backend/src/Activities.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Activities.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Activities.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> contextOptions) : base(contextOptions)
+        {
+        }
+        
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
--- backend/src/Activities.API/Startup.cs
+++ backend/src/Activities.API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Activities.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,8 @@ namespace Activities.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("Default")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -50,10 +53,7 @@ namespace Activities.API
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
