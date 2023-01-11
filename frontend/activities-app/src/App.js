1937c176606ba22401857dc24e519f34b54b5da5
Rodrigo Leonardo <rodrigoleonardo81@gmail.com>
2023-01-10 21:33:02 -0300

    adding client components

--- frontend/activities-app/src/App.js
+++ frontend/activities-app/src/App.js
@@ -1,21 +1,15 @@
 import "./App.css";
 import Activity from "./pages/activities/Activity";
 import { Route, Switch } from "react-router-dom";
+import Client from './pages/clients/Client';
+import Dashboard from './pages/dashboard/Dashboard';
 
 export default function App() {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/activities" component={Activity} />
+      <Route path="/dashboard" exact component={Dashboard} />
+      <Route path="/activities" exact component={Activity} />
       <Route path="/clients" component={Client} />
     </Switch>
   );
 }
-
-const Home = () => {
-  return <div>opa </div>;
-};
-
-const Client = () => {
-  return <div>aaaaaaaaaaaS</div>;
-};
---
+++ frontend/activities-app/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TitlePage from './../../components/TitlePage';
+
+export default function Dashboard() {
+    return (
+        <>
+            <TitlePage title="Dashboard"/>
+            <div>
+                cards
+            </div>
+        </>
+    )
+}
